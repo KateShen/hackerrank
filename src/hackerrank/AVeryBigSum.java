0c23d19dfe6e04b0576382296cd5f6439d71ba71
yushen <215649498@qq.com>
2017-03-29 23:59:06 +1100

    Long

--- src/hackerrank/AVeryBigSum.java
+++ src/hackerrank/AVeryBigSum.java
@@ -1,7 +1,18 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 public class AVeryBigSum {
 	 public static void main(String[] args) {
 	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		 Scanner sc=new Scanner(System.in);
+		 int length_array=sc.nextInt();
+		 long count = 0;
+		 for (int i = 0; i < length_array; i++) {
+			long b=sc.nextLong();
+			count=count+b;
+		}
+		 System.out.println(count);
+		 
 	    }
 }
